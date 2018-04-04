80ab00c4276aaebea539c95349c18db57d1bb02f
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-04 10:54:14 -0400

    2. Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_page'
+
+  get 'static_pages/index'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
