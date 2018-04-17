a4628eb87cb4bc10a8c3391af0273b85ef6ce80a
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-17 07:06:36 -0400

    17/4/18 migrate video url column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412110632) do
+ActiveRecord::Schema.define(version: 20180417093052) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180412110632) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video_url"
   end
 
 end
